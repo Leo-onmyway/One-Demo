28121ebc809242da99cf964402e842511465d0c4
Leo <leo@abc.efg>
2012-10-28 00:02:56 +0800

    Add all four items to make suit

--- yiku/app/models/shoe.rb
+++ yiku/app/models/shoe.rb
@@ -1,5 +1,6 @@
 class Shoe < ActiveRecord::Base
   
-    has_attached_file :photo, :styles => { :thumb => ["100x100>", :png], :original => ["400x400>", :jpg] }, :whiny => false
+      has_attached_file :photo, :styles => { :thumb => ["100x100>", :png], :small => ["100x100", :jpg], :original => ["400x400>", :jpg] }, :convert_options => {
+          :thumb => "-bordercolor white -border 1x1 -alpha set -channel RGBA -fuzz 20% -fill none -floodfill +0+0 white -shave 1x1" }, :whiny => false
 
 end
